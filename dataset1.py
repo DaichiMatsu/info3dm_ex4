dfecf62aba6b37427a298b31bb347802b41a09c8
Matsumoto Daichi <e245759@ie.u-ryukyu.ac.jp>
2026-04-30 16:03:18 +0900

    2nd

--- dataset1.py
+++ dataset1.py
@@ -5,24 +5,31 @@ font_path = '/Library/Fonts/Arial Unicode.ttf'
 font_prop = font_manager.FontProperties(fname = font_path)
 matplotlib.rcParams['font.family'] = font_prop.get_name()
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
+# 観測点設定
+np.random.seed(0)
+n = 20
+x_obs = np.random.uniform(-1, 1, n)
+
+# 真値
 def true_function(x):
     x = np.array(x)
     return np.sin(np.pi * x * 0.8) * 10
 
-# x=0の時のyの値のテスト
-y = true_function(0)
-print(y)
+y_true = true_function(x_obs)
 
+# DataFrame型
+DF = pd.DataFrame({"観測点": x_obs, "真値": y_true})
 
-# -1<=x<=1のxyグラフ
-import matplotlib.pyplot as plt
-
+# サンプル集合をプロット
 x = np.linspace(-1, 1, 100)
 y = true_function(x)
-
 plt.plot(x, y, label="y = 10sin(0.8πx)")
+plt.scatter(DF["観測点"], DF["真値"], label="サンプル集合", s=50)
 plt.xlim(-1, 1)
 plt.legend()
-plt.savefig("ex1.1.png")
+plt.savefig("ex1.2.png")
 plt.show()
+
